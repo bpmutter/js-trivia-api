5d9b4fc5e54bdd1b3d47a71b9c9dd81b61eb0203
Ben Perlmutter <benperlmutter94@gmail.com>
2020-08-01 22:44:56 -0500

    add spanish parser and routes

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const fs = require("fs");
 const asyncHandler = require("../utils/asyncHandler");
-const questionsEN = require("../questions/en-EN/en.json");
-console.log(questionsEN);
 
 const router = express.Router();
 
@@ -17,21 +15,34 @@ router.get("/:id(\\d+)", (req, res) => {
 });
 
 
-router.get("/en", asyncHandler(async(req, res) => {
+router.get("/:lang", asyncHandler(async(req, res) => {
+    const lang = req.params.lang;
+    try{
+      const questions = require(`../questions/${lang}/${lang}.json`);
+      res.send({ questions });
+    }catch(err){
+      console.error(err);
+      res.status(500).send({ error: "Language not supported.", status: 500 });
+    }
     
-    res.send({questions})
 }));
-router.get("/en/:id(\\d+)", asyncHandler(async(req, res) => {
+router.get("/:lang/:id(\\d+)", asyncHandler(async(req, res) => {
+    const lang = req.params.lang;
     const id = parseInt(req.params.id);
+    try{
+      const questions = require(`../questions/${lang}/${lang}.json`);
+      const question = questions[id - 1];
+      if (question) res.send({ question });
+      else
+        res.status(404).send({
+          error: `The requested resource ID ${id} could not be found.`,
+          status: 404,
+        });
+    }catch(err){
+      console.error(err);
+      res.status(500).send({ error: "An unexpected server error occurred.", status: 500 });
+    }
     
-    const question = questionsEN[id-1];
-    if(question) res.send({question});
-    else res
-           .status(404)
-           .send({
-             error: `The requested resource ID ${id} could not be found.`,
-             status: 404
-           });
 }));
 
 module.exports = router;
