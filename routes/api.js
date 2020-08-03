0165f58cfff551dec2206e3e5018e20bba74421f
Ben Perlmutter <benperlmutter94@gmail.com>
2020-08-03 17:27:18 -0500

    add german language support and refactor route fetching data

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ const asyncHandler = require("../utils/asyncHandler");
 
 const router = express.Router();
 
+const supportedLangs = ["en", "es", "fr", "de"];
 
 //redirect generic queries to English results
 router.get("/", (req, res) => {
@@ -18,11 +19,17 @@ router.get("/:id(\\d+)", (req, res) => {
 router.get("/:lang", asyncHandler(async(req, res) => {
     const lang = req.params.lang;
     try{
+      if(supportedLangs.indexOf(lang)<0) throw new Error("Language not supported")
       const questions = require(`../questions/${lang}/${lang}.json`);
       res.send({ questions });
     }catch(err){
       console.error(err);
-      res.status(500).send({ error: "Language not supported.", status: 500 });
+      res
+        .status(404)
+        .send({
+          error: `Language identifier '${lang}' is not supported.`,
+          status: 404,
+        });
     }
     
 }));
@@ -30,14 +37,21 @@ router.get("/:lang/:id(\\d+)", asyncHandler(async(req, res) => {
     const lang = req.params.lang;
     const id = parseInt(req.params.id);
     try{
+      if (supportedLangs.indexOf(lang) < 0)
+        return res.status(404).send({
+          error: `Language identifier '${lang}' is not supported.`,
+          status: 404,
+        });
       const questions = require(`../questions/${lang}/${lang}.json`);
       const question = questions[id - 1];
-      if (question) res.send({ question });
-      else
-        res.status(404).send({
+      if (question) return res.send({ question });
+      else{
+        return res.status(404).send({
           error: `The requested resource ID ${id} could not be found.`,
           status: 404,
         });
+      }
+      
     }catch(err){
       console.error(err);
       res.status(500).send({ error: "An unexpected server error occurred.", status: 500 });
