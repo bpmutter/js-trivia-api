8a8a83bbc50e45e6baff53773f8ca91fe0643173
James Robertson <jamesurobertson@gmail.com>
2020-08-05 22:24:13 -0500

    intial mongo setup

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    remoteUrl :   "mongodb+srv://DBUser:jstrivia1!@cluster0.nkzjp.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    localUrl: ''
+};
--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const express = require('express');
+// set up -----------------------------------------------------
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongodb");
+const port = process.env.PORT || 8080;
+const database = require('./config/database')
+const morgan = require('morgan')
+const bodyParser = require('body-parser');
 const cors = require("cors");
 
-const port = process.env.PORT || 8080;
-const apiRouter = require('./routes/api');
-const app = express();
-
+// configuration ----------------------------------------------
+mongoose.connect(database.remoteUrl)
 app.use(cors());
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+
+// routes -----------------------------------------------------
+require("./routes/api")(app);
 
 
-app.use("/", apiRouter);
-
+// listen -----------------------------------------------------
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
