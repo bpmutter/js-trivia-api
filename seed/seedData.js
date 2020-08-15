7b50b1189b285cda13640bf7373204cc3b1a5921
Ben Perlmutter <benperlmutter94@gmail.com>
2020-08-15 17:23:02 -0500

    fix seeder to delete old data

--- seed/seedData.js
+++ seed/seedData.js
@@ -1,16 +1,17 @@
 const LanguageModels = require('../models/LanguageModels')
 const mongoose = require('mongoose')
 const supportedLanguages = require('../supportedLanguages')
+require("dotenv").config();
+const remoteURl = "mongodb+srv://js-trivia:jstrivia1!@cluster0.m6q07.mongodb.net/jstrivia_app?retryWrites=true&w=majority";
 
-
-mongoose.connect(process.env.REMOTE_DB_URL, {
+mongoose.connect(remoteURl, {
     useUnifiedTopology: true
 })
 
 
 supportedLanguages.forEach((lang, i) => {
     const questions = require(`../questions/${lang}/${lang}.json`)
-
+    LanguageModels[i].collection.remove({});
     LanguageModels[i].collection.insertMany(questions, (err, result) => {
         if (err) return console.error(err)
         else console.log(result)
